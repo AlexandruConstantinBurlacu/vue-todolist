cfc9dfd990bdafde0eab11d1c92740af992749d2
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-20 14:53:15 +0100

    creazione array con cose da fare

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            tasks: [
+                "Passeggiata nella natura",
+                "Visita un museo",
+                "Serata di giochi da tavolo",
+                "Cucinare un nuovo piatto",
+                "Leggere un libro avvincente",
+            ]
         }
     },
 }).mount('#app');
